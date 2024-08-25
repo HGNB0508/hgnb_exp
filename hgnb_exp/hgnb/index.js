36a8aa170e8ba1472025609cde8cf7d6378a62df
HGNB <78014392+HGNB0508@users.noreply.github.com>
2024-08-25 15:12:02 +0900

    Add files via upload

---
+++ hgnb_exp/hgnb/index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src = 'https://dimg.donga.com/wps/NEWS/IMAGE/2019/05/28/95731526.2.jpg'
+})
+
+}, 100)
